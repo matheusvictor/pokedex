749f9475fa4dbb64f04c64a0f39db35f9f8c1eab
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-05 11:41:46 -0300

    Add função pata obter mais detalhes de um Pokémon
    - Ao clicar num Pokémon, uma requisição é feita para obter + detalhes

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -24,7 +24,34 @@ pokeApi.getPokemonDetail = (pokemon) => {
                             .then(convertPokeApiDetailsToPokemon);
 }
 
-pokeApi.getAll = (offset = 0, limit = 5) => {
+pokeApi.getMoreDetailsOfPokemon = (pokemonNumber) => {
+    const URL = `https://pokeapi.co/api/${apiVersion}/${source}/${pokemonNumber}`;
+
+    return fetch(URL)
+                    .then((response) => response.json())
+                    .then((data) => {
+
+                        console.log(data)
+                        let stats = data.stats.map(stat => stat.base_stat);
+
+                        const otherDetails = {
+                            height: (data.height) / 10,
+                            weight: data.weight / 10,
+                            hp: data.stats.find((stat) => stat.stat.name).base_stat,
+                            attack: stats[1],
+                            defense: stats[2],
+                            specialAttack: stats[3],
+                            specialDefense: stats[4],
+                            speed: stats[5]
+                        };
+
+                        console.log(otherDetails)
+                        alert(`Pokémon: ${data.name}\nPeso: ${otherDetails.weight},\nAltura: ${otherDetails.height}`);
+                    })
+                    .catch((error) => console.log(error));
+}
+
+pokeApi.getAllPokemons = (offset = 0, limit = 5) => {
     const URL = `https://pokeapi.co/api/${apiVersion}/${source}?offset=${offset}&limit=${limit}`;
     
     return fetch(URL)
